c932277f46c6eac344f92f4e81dbdaad42286305
tungbi811 <tungduynguyen811@gmail.com>
2026-01-23 16:24:52 +1100

    update database

--- etl/load/writer.py
+++ etl/load/writer.py
@@ -2,6 +2,10 @@ import json
 import asyncio
 from pathlib import Path
 from config.settings import *
+from config.config import load_config
+import psycopg2
+from loader import load_batch
+from utils.insert import insert_products_and_images
 
 failed_products = {}
 failed_lock = asyncio.Lock()
@@ -35,3 +39,14 @@ async def save_batch(batch_id: int, products: list):
     }
     path = FOLDER / f"batch_{batch_id:04d}.json"
     await asyncio.to_thread(write_json_sync, path, payload)
+
+def write_to_db():
+    config = load_config()
+
+    with psycopg2.connect(**config) as conn:
+        for path in sorted(Path("data/tiki_products").glob("batch_*.json")):
+            product_list, image_list = load_batch(path)
+
+            insert_products_and_images(product_list, image_list)
+
+            print(f"[DB] loaded {path.name}")
